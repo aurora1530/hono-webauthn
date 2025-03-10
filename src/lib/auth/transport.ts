908aa06ed8cdb11566d11551f193da2b1917e370
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-10 11:42:02 +0000

    feat: AuthenticatorTransportFutureの型ガードを追加

---
+++ src/lib/auth/transport.ts
@@ -0,0 +1,12 @@
+import type { AuthenticatorTransportFuture } from '@simplewebauthn/server';
+
+export const isAuthenticatorTransportFuture = (
+  target: unknown
+): target is AuthenticatorTransportFuture => {
+  if (typeof target !== 'string') {
+    return false;
+  }
+  return ['ble', 'cable', 'hybrid', 'internal', 'nfc', 'smart-card', 'usb'].includes(
+    target
+  );
+};
