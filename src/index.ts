5bca6c0188ae408b70a5e95e0c3362a7c55eaabc
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-10 05:43:21 +0000

    refactor: format

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { serve } from '@hono/node-server'
-import { serveStatic } from '@hono/node-server/serve-static'
-import { Hono } from 'hono'
-import { logger } from 'hono/logger'
-import { secureHeaders } from 'hono/secure-headers'
-import { trimTrailingSlash } from 'hono/trailing-slash'
+import { serve } from '@hono/node-server';
+import { serveStatic } from '@hono/node-server/serve-static';
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { secureHeaders } from 'hono/secure-headers';
+import { trimTrailingSlash } from 'hono/trailing-slash';
 
-const app = new Hono()
+const app = new Hono();
 
 app.use(logger());
 app.use(trimTrailingSlash());
@@ -13,12 +13,15 @@ app.use(serveStatic({ root: 'public' }));
 app.use(secureHeaders());
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
