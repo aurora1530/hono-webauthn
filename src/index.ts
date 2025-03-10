2961f40797e97e1c6512901bcecbc003f5b91347
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-10 05:42:46 +0000

    feat: 基本的なmiddlewareを追加

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import { secureHeaders } from 'hono/secure-headers'
+import { trimTrailingSlash } from 'hono/trailing-slash'
 
 const app = new Hono()
+
+app.use(logger());
+app.use(trimTrailingSlash());
+app.use(serveStatic({ root: 'public' }));
+app.use(secureHeaders());
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
