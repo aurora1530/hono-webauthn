612fc433ace94c486f68b30a616f1817d44646c4
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-19 15:22:53 +0900

    feat: アプリケーションを高さ固定で実行できるように

--- src/rootRenderer.tsx
+++ src/rootRenderer.tsx
@@ -1,17 +1,38 @@
-import { jsxRenderer } from 'hono/jsx-renderer';
+import { jsxRenderer, useRequestContext } from 'hono/jsx-renderer';
 import Header from './components/common/Header.js';
+import { css, cx, Style } from 'hono/css';
 
 const rootRenderer = jsxRenderer(({ children, title }) => {
+  const c = useRequestContext();
+  const htmlClass = css`
+    margin:0;
+    padding:0;
+  `
+
+  const bodyClass = cx(
+    htmlClass,
+    css`
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+    `
+  )
+
+  const mainClass = css`
+    flex: 1;
+  `
+
   return (
-    <html lang="ja">
+    <html lang="ja" class={htmlClass}>
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
+        <Style/>
       </head>
-      <body>
+      <body class={bodyClass}>
         <Header />
-        {children}
+        <main class={mainClass}>{children}</main>
       </body>
     </html>
   );
