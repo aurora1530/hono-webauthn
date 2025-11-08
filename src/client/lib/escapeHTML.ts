c310c4b94baea33d15232433c53a67abd60a0c1e
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-11-08 17:17:55 +0900

    feat: HTMLエスケープ用の関数を追加

---
+++ src/client/lib/escapeHTML.ts
@@ -0,0 +1,7 @@
+export const escapeHTML = (s: string): string => {
+  return s.replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
