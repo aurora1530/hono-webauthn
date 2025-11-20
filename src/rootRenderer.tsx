6819ba886fd014b4bf51bb5ffc8b8241c64872af
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-11-20 23:56:37 +0900

    feat: material symbolsを追加

--- src/rootRenderer.tsx
+++ src/rootRenderer.tsx
@@ -48,6 +48,10 @@ const rootRenderer = jsxRenderer(({ children, title }) => {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+          rel="stylesheet"
+        />
         <Style />
       </head>
       <body class={bodyClass}>
