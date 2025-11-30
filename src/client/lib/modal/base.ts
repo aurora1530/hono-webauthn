fa86ed2c009ecb777e1e194c7e767f31007cec19
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-11-30 12:39:20 +0900

    feat(modal): closeModalとopenModalWithJSXにoverwriteイベントの処理を追加

--- src/client/lib/modal/base.ts
+++ src/client/lib/modal/base.ts
@@ -21,20 +21,39 @@ function openModal(contentElement: HTMLElement, onClose?: () => void) {
 
 function closeModal() {
   const modal = document.getElementById("main-modal") as HTMLDialogElement | null;
+  const modalContent = document.getElementById("main-modal-content");
   if (modal) {
+    // closeModal呼び出し時は、ここで内容をクリアする。これにより、overwriteイベントと、正常にcloseされた場合とで区別できる。
+    if (modalContent) modalContent.innerHTML = "";
     modal.close();
   }
 }
 
 type ModalEventHandlers = {
   onClose?: () => void;
+  onOverwrite?: () => void;
 };
 
 function openModalWithJSX(content: Child, handlers?: ModalEventHandlers) {
   const modal = document.getElementById("main-modal") as HTMLDialogElement | null;
   const modalContent = document.getElementById("main-modal-content");
   if (modal && modalContent) {
+    // 別のモーダルが存在する場合、overwriteイベントを発火させる。
+    if (modalContent.firstChild) {
+      const overwriteEvent = new Event("overwrite");
+      modalContent.firstChild.dispatchEvent(overwriteEvent);
+    }
+
     render(content, modalContent);
+
+    // イベントハンドラを登録する
+    if (handlers?.onOverwrite) {
+      const overwriteHandler = () => {
+        handlers.onOverwrite!();
+        modalContent.firstChild?.removeEventListener("overwrite", overwriteHandler);
+      };
+      modalContent.firstChild?.addEventListener("overwrite", overwriteHandler);
+    }
     if (handlers?.onClose) {
       const closeHandler = () => {
         handlers.onClose!();
